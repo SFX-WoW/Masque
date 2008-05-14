18acb75fd05d1cdb87498047e93fa08b0ce07e9c
StormFX <stormfx@gmail.com>
2008-05-14 22:16:23 +0000

    - Tweak Autocast. - Add color selection support. - Minor tweaks.

--- Skins/Skins.lua
+++ Skins/Skins.lua
@@ -1,75 +1,80 @@
-
 local LibButtonFacade = LibStub("LibButtonFacade",true)
 if not LibButtonFacade then
 	return
 end
 
-LibButtonFacade:AddSkin("DreamLayout",{
+-- Dream Layout
+LibButtonFacade:AddSkin("Dream Layout",{
 	Backdrop = {
 		Width = 36,
 		Height = 36,
+		Color = {0, 0, 0, 0.6},
 		Texture = [[Interface\Tooltips\UI-Tooltip-Background]],
-		Red = 0,
-		Green = 0,
-		Blue = 0,
-		Alpha = 0.6,
 	},
 	Icon = {
 		Width = 30,
 		Height = 30,
 		TexCoords = {0.07,0.93,0.07,0.93},
 	},
-	Border = {
-		Width = 62,
-		Height = 62,
-		Texture = [[Interface\Buttons\UI-ActionButton-Border]],
-		BlendMode = "ADD",
-	},
 	Flash = {
-		Width = 36,
-		Height = 36,
+		Width = 32,
+		Height = 32,
+		Color = {1, 1, 1, 1},
 		Texture = [[Interface\Buttons\UI-QuickslotRed]],
 	},
 	Cooldown = {
-		Width = 36,
-		Height = 36,
+		Width = 32,
+		Height = 32,
 	},
 	AutoCast = {
-		Width = 36,
-		Height = 36,
-		ModelX = 0.002,
-		ModelY = 0.002,
-	},
-	AutoCastable = {
-		Width = 58,
-		Height = 58,
-		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+		Width = 30,
+		Height = 30,
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+		ModelScale = 0.975,
 	},
 	Normal = {
 		Hide = true,
 	},
 	Pushed = {
-		Width = 36,
-		Height = 36,
+		Width = 32,
+		Height = 32,
+		Color = {1, 1, 1, 1},
 		Texture = [[Interface\Buttons\UI-Quickslot-Depress]],
+	},
+	Border = {
+		Width = 56,
+		Height = 56,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\UI-ActionButton-Border]],
 	},
 	Disabled = {
 		Hide = true,
 	},
 	Checked = {
-		Width = 36,
-		Height = 36,
-		Texture = [[Interface\Buttons\CheckButtonHilight]],
+		Width = 32,
+		Height = 32,
 		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\CheckButtonHilight]],
+	},
+	AutoCastable = {
+		Width = 54,
+		Height = 54,
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+	},
+	Highlight = {
+		Width = 32,
+		Height = 32,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\ButtonHilight-Square]],
 	},
 	Gloss = {
 		Hide = true
-	},
-	Highlight = {
-		Width = 36,
-		Height = 36,
-		Texture = [[Interface\Buttons\ButtonHilight-Square]],
-		BlendMode = "ADD",
 	},
 	HotKey = {
 		Width = 36,
@@ -90,8 +95,8 @@ LibButtonFacade:AddSkin("DreamLayout",{
 	},
 },true)
 
+-- Zoomed
 LibButtonFacade:AddSkin("Zoomed",{
-	Template = "DreamLayout",
 	Backdrop = {
 		Hide = true,
 	},
@@ -99,5 +104,81 @@ LibButtonFacade:AddSkin("Zoomed",{
 		Width = 36,
 		Height = 36,
 		TexCoords = {0.07,0.93,0.07,0.93},
+	},
+	Flash = {
+		Width = 36,
+		Height = 36,
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\UI-QuickslotRed]],
+	},
+	Cooldown = {
+		Width = 36,
+		Height = 36,
+	},
+	AutoCast = {
+		Width = 36,
+		Height = 36,
+		OffsetX = 0.5,
+		ModelScale = 1.15,
+	},
+	Normal = {
+		Hide = true,
+	},
+	Pushed = {
+		Width = 36,
+		Height = 36,
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\UI-Quickslot-Depress]],
+	},
+	Border = {
+		Width = 64,
+		Height = 64,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\UI-ActionButton-Border]],
+	},
+	Disabled = {
+		Hide = true,
+	},
+	Checked = {
+		Width = 36,
+		Height = 36,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\CheckButtonHilight]],
+	},
+	AutoCastable = {
+		Width = 66,
+		Height = 66,
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+	},
+	Highlight = {
+		Width = 36,
+		Height = 36,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 1},
+		Texture = [[Interface\Buttons\ButtonHilight-Square]],
+	},
+	Gloss = {
+		Hide = true
+	},
+	HotKey = {
+		Width = 36,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = 11,
+	},
+	Count = {
+		Width = 36,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = -11,
+	},
+	Name = {
+		Width = 36,
+		Height = 10,
+		OffsetY = -11,
 	},
 },true)
