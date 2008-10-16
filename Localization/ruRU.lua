094cce61e67a3e122e37b2e23e0eba3c3a862cb1
swix <swix@wowace.com>
2008-10-16 20:34:38 +0000

    - Added ruRU locale.

---
+++ Localization/ruRU.lua
@@ -0,0 +1,38 @@
+﻿local AceLocale = LibStub("AceLocale-3.0")
+
+--ruRU by wow.playhard.ru team
+local L = AceLocale:NewLocale( "ButtonFacade", "ruRU")
+
+if L then
+	L["BF"] = true -- Short for ButtonFacade
+	L["ButtonFacade"] = true
+	L["|cffffff00Right-click|r to open the Configuration GUI"] = "|cffffff00ПКЛ|r для открытия ГИП настроек"
+	L["Apply skin to all registered buttons."] = "Применить шкурку ко всем зарегистрированным кнопкам."
+	L["Apply skin to all buttons registered with %s."] = "Применить шкурку ко всем кнопкам зарегистрированным с %s."
+	L["Apply skin to all buttons registered with %s/%s."] = "Применить шкурку ко всем кнопкам зарегистрированным с%s/%s."
+	L["Apply skin to all buttons registered with %s/%s/%s."] = "Применить шкурку ко всем кнопкам зарегистрированным с %s/%s/%s."
+	L["Elements"] = "Элементы"
+	L["Skin"] = "Шкурки"
+	L["Gloss"] = "Глянец"
+	L["Backdrop"] = "Фон"
+	L["FuBar options"] = "Опции FuBarа"
+	L["Attach to minimap"] = "Закрепить у мини-карты"
+	L["Hide minimap/FuBar icon"] = "Скрыть иконку у мини-курты/FuBarа"
+	L["Show icon"] = "Показ иконки"
+	L["Show text"] = "Показ текста"
+	L["Position"] = "Позиция"
+	L["Left"] = "Слава"
+	L["Center"] = "По центру"
+	L["Right"] = "Справо"
+	L["Color Options"] = "Опции цвета"
+	L["Backdrop"] = "Фон"
+	L["Flash"] = "Сверкание"
+	L["Normal Border"] = "Нормальные края"
+	L["Pushed Border"] = "Вдавленные края"
+	L["Disabled Border"] = "Отключить края"
+	L["Checked"] = "Проверенный"
+	L["Equipped"] = "Задействованный"
+	L["Highlight"] = "Выделение"
+	L["Gloss"] = "Глянец"
+	L["Reset Colors"] = "Сбросить цвета"
+end
