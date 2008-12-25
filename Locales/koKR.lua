7b535cc657dc61be5daf3e46fd44b104e4e50cf1
next96 <next96@wowace.com>
2008-12-25 16:56:08 +0000

    - ButtonFacade : - Updated koKR locale.

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -5,15 +5,15 @@ if L then
 	L["ButtonFacade"] = "ButtonFacade"
 
 	-- Broker Tool Tip
-	L["Right-Click to open the options window."] = "Right-Click to open the options window."
+	L["Right-Click to open the options window."] = "오른쪽 클릭으로 설정창을 엽니다."
 
 	-- Global Settings
-	L["Global"] = "Global"
-	L["GLOBAL_INFO"] = "This section allows you adjust the skin settings globally. Any changes here will affect all registered elements. Please note that this section will not update itself after a reload."
+	L["Global"] = "글로벌"
+	L["GLOBAL_INFO"] = "전체 스킨에 적용하는 설정입니다. 모든 변경사항이 전체에 영향을 받습니다."
 
 	-- Addon Settings
-	L["Addons"] = "Addon"
-	L["ADDON_INFO"] = "This section allows you adjust skin settings on a per-addon basis. You can also adjust the settings of individual groups, bars and buttons of the addon if available."
+	L["Addons"] = "애드온"
+	L["ADDON_INFO"] = "개별적인 애드온에 적용하는 설정입니다. 개인적인 그룹, 바 또는 버튼에 대해서 설정을 변경할 수 있습니다."
 
 	-- Elements
 	L["Apply skin to all buttons registered with %s."] = "%s에 등록된 모든 버튼에 스킨을 적용합니다."
@@ -36,29 +36,29 @@ if L then
 	L["Reset Colors"] = "색상 초기화"
 
 	-- General Options
-	L["Options"] = "Options"
-	L["OPTION_INFO"] = "This section allows you to adjust any options that are available for ButtonFacade."
-	L["Minimap Icon"] = "Minimap Icon"
-	L["Show the minimap icon."] = "Show the minimap icon."
-	L["OPTWIN_ISSUE"] = "If you're having trouble accessing some of the options due to the window size, you may want to download and install |cffffcc00BetterBlizzOptions|r. Alternatively, you can use the button below or the |cffffcc00/bfo|r chat command to open a standalone options window."
-	L["Standalone Options"] = "Standalone Options"
-	L["Open a standalone options window."] = "Open a standalone options window."
+	L["Options"] = "설정"
+	L["OPTION_INFO"] = "ButtonFacade의 설정창입니다."
+	L["Minimap Icon"] = "미니맵 아이콘"
+	L["Show the minimap icon."] = "미니맵 아이콘을 표시합니다."
+	L["OPTWIN_ISSUE"] = "창의 크기에 의존하는 설정을 불러오는데 문제가 없다면, |cffffcc00BetterBlizzOptions|r 애드온 설치를 추천합니다. 미니맵 버튼을 이용하거나 |cffffcc00/bfo|r 입력하면 단독 설정창을 실행합니다."
+	L["Standalone Options"] = "단독 설정"
+	L["Open a standalone options window."] = "단독 설정창을 엽니다."
 	
 	-- Plugins
-	L["Plugins"] = "Plugins"
-	L["PLUGIN_INFO"] = "This section allows you adjust the settings of individual plugins."
+	L["Plugins"] = "플러그인"
+	L["PLUGIN_INFO"] = "개별적인 플러그인에 적용하는 설정입니다."
 
 	-- Profiles
 	L["Profiles"] = "Profiles"
 
 	-- About
-	L["About"] = "About"
-	L["BF_INFO"] = "ButtonFacade is a small addon that allows the dynamic skinning of button-based addons."
-	L["Version"] = "Version"
-	L["Authors"] = "Authors"
-	L["Web Site"] = "Web Site"
-	L["Feedback"] = "Feedback"
-	L["FB_TEXT"] = "If you have questions or comments or would like to submit a bug, please visit the project page and submit a ticket. You will also find a list of frequently asked questions covering the most common issues."
-	L["Translations"] = "Translations"
-	L["TRANS_TEXT"] = "If you would like to help translate ButtonFacade, please visit the project page and follow the directions for submitting translations."
+	L["About"] = "대하여"
+	L["BF_INFO"] = "ButtonFacade는 버튼을 표시하는 애드온에 스킨을 씌워주는 애드온입니다."
+	L["Version"] = "버전"
+	L["Authors"] = "제작자"
+	L["Web Site"] = "홈페이지"
+	L["Feedback"] = "피드백"
+	L["FB_TEXT"] = "애드온에 대한 질문이나 건의사항 또는 버그가 있으면 홈페이지에 방문해서 글을 남겨주십시오. 대부분의 사용에 관한 내용은 FAQ를 참조하세요."
+	L["Translations"] = "번역"
+	L["TRANS_TEXT"] = "ButtonFacade의 지역화 파일을 수정하려면, 홈페이지에 방문해서 지역화 파일을 등록하면 됩니다."
 end
