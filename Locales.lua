d7f9dfc244c060a379dceccafe747aecd2654228
StormFX <stormfx@gmail.com>
2009-01-31 15:08:07 +0000

    - Testing auto-injection of locales again.

---
+++ Locales.lua
@@ -0,0 +1,58 @@
+-- ButtonFacade/Locales
+-- This contents of this file are automatically generated.
+-- Please use the localization application on WoWAce.com to submit translations.
+-- http://www.wowace.com/projects/buttonfacade/localization/
+
+local AL3 = LibStub("AceLocale-3.0")
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = AL3:NewLocale("ButtonFacade", "enUS", true, debug)
+if L then
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, table-name="L")@
+if GetLocale() == "enUS" or GetLocale() == "enGB" then return end
+end
+
+local L = AL3:NewLocale("ButtonFacade", "deDE")
+if L then
+--@localization(locale="deDE", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "esES") or AL3:NewLocale("ButtonFacade", "esMX")
+if L then
+--@localization(locale="esES", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "frFR")
+if L then
+--@localization(locale="frFR", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "koKR")
+if L then
+--@localization(locale="koKR", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "ruRU")
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "zhCN")
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
+
+local L = AL3:NewLocale("ButtonFacade", "zhTW")
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+return
+end
