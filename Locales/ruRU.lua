3f79e336e10852b83827b541ba8a3aa2766eb99a
StormFX <stormfx@gmail.com>
2018-07-23 16:17:08 -0500

    - Embed ruRU translations.

---
+++ Locales/ruRU.lua
@@ -0,0 +1,72 @@
+--[[
+	This file is part of 'Masque', an add-on for World of Warcraft. For license information,
+	please see the included License.txt file.
+
+	* File...: Locales\ruRU.lua
+
+	[ Notes ]
+
+	To help with translations, use the localization system on WoWAce (https://www.wowace.com/projects/masque/localization)
+	or contribute directly on GitHub (https://github.com/stormfx/masque).
+]]
+
+if GetLocale() ~= "ruRU" then return end
+
+local _, Core = ...
+local L = Core.Locale
+
+L["Addons"] = "Модификации"
+L["Adjust the skin of all buttons registered to %s. This will overwrite any per-group settings."] = "Позволяет редактировать скины всех панелей, под управлением %s. Это переопределит все групповые настройки."
+L["Adjust the skin of all buttons registered to %s: %s."] = "Позволяет редактировать скины всех панелей, под управлением %s: %s."
+L["Adjust the skin of all registered buttons. This will overwrite any per-add-on settings."] = "Позволяет редактировать скины всех доступных панелей. Это переопределит все настройки отдельных аддонов."
+L["Backdrop Settings"] = "Настройки фона"
+L["Causes Masque to throw Lua errors whenever it encounters a problem with an add-on or skin."] = "Заставляет Masque выдавать ошибки Lua, с чем бы они не были связаны: аддоном или скином."
+L["Checked"] = "При выделении"
+L["Click this button to load Masque's options. You can also use the %s or %s chat command."] = "Нажмите эту кнопку, чтобы загрузить настройки Masque. Также можно использовать команды %s или %s."
+L["Click to open Masque's options window."] = "ЛКМ для открытия настроек Masque."
+L["Color"] = "Цвет"
+L["Colors"] = "Цвета"
+--[[Translation missing --]]
+L["Cooldown"] = "Cooldown"
+L["Debug Mode"] = "Режим отладки"
+L["Disable"] = "Отключить"
+L["Disable the skinning of this group."] = "Отключить скины для этой группы."
+L["Disabled"] = "Отключенный"
+L["Enable"] = "Включить"
+L["Enable the Backdrop texture."] = "Включить настройки фона текстуры."
+L["Enable the Minimap icon."] = "Отображать иконку у миникарты."
+--[[Translation missing --]]
+L["Equipped"] = "Equipped"
+L["Flash"] = "Сверкание"
+L["General"] = "Общее"
+L["Global"] = "Общее"
+L["Gloss Settings"] = "Настройки глянца"
+L["Highlight"] = "При наведении"
+L["Load Masque Options"] = "Загрузить настройки"
+L["Loading Masque Options..."] = "Загрузка настроек..."
+L["Masque debug mode disabled."] = "Режим отладки Masque отключен."
+L["Masque debug mode enabled."] = "Режим отладки Masque включен."
+L["Masque is a dynamic button skinning add-on."] = "Masque - аддон, позволяющий добавлять скины к панелям команд."
+L["Minimap Icon"] = "Иконка у миникарты"
+L["Normal"] = "Нормальный"
+L["Opacity"] = "Непрозрачность"
+L["Profiles"] = "Профили"
+L["Pushed"] = "При нажатии"
+L["Reset all skin options to the defaults."] = "Установить значения цветов по умолчанию."
+L["Reset Skin"] = "Сбросить цвета"
+L["Set the color of the Backdrop texture."] = "Установить цвет фона текстуры."
+L["Set the color of the Checked texture."] = "Установить цвет текстуры при выделении."
+--[[Translation missing --]]
+L["Set the color of the Cooldown animation."] = "Set the color of the Cooldown animation."
+L["Set the color of the Disabled texture."] = "Установить цвет отключенной текстуры."
+--[[Translation missing --]]
+L["Set the color of the Equipped item texture."] = "Set the color of the Equipped item texture."
+L["Set the color of the Flash texture."] = "Установить цвет текстуры с подсветкой."
+L["Set the color of the Gloss texture."] = "Задать цвет для глянца текстур."
+L["Set the color of the Highlight texture."] = "Установить цвет текстуры при наведении."
+L["Set the color of the Normal texture."] = "Установить нормальный цвет текстуры."
+L["Set the color of the Pushed texture."] = "Установить цвет текстуры при нажатии."
+L["Set the intensity of the Gloss color."] = "Установить интенсивность глянца."
+L["Set the skin for this group."] = "Установить скин для данной группы."
+L["Skin"] = "Скины"
+L["This section will allow you to skin the buttons of the add-ons and add-on groups registered with Masque."] = "Этот раздел позволяет настроить скины для панелей команд аддонов и групп аддонов, использующих Masque."
