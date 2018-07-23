417130fdf89af677c1eba1a051019e3d2d22d853
StormFX <stormfx@gmail.com>
2018-07-23 16:05:52 -0500

    - Embed deDE translations.

---
+++ Locales/deDE.lua
@@ -0,0 +1,68 @@
+--[[
+	This file is part of 'Masque', an add-on for World of Warcraft. For license information,
+	please see the included License.txt file.
+
+	* File...: Locales\deDE.lua
+
+	[ Notes ]
+
+	To help with translations, use the localization system on WoWAce (https://www.wowace.com/projects/masque/localization)
+	or contribute directly on GitHub (https://github.com/stormfx/masque).
+]]
+
+if GetLocale() ~= "deDE" then return end
+
+local _, Core = ...
+local L = Core.Locale
+
+L["Addons"] = "Addons"
+L["Adjust the skin of all buttons registered to %s. This will overwrite any per-group settings."] = "Passe die Optik aller Buttons, die auf %s registriert sind, an. Dies überschreibt alle gruppenspezifischen Einstellungen."
+L["Adjust the skin of all buttons registered to %s: %s."] = "Passe die Optik aller Buttons, die auf %s: %s registriert sind, an."
+L["Adjust the skin of all registered buttons. This will overwrite any per-add-on settings."] = "Passe den Optik aller registrierten Buttons an. Dies überschreibt alle addonspezifischen Einstellungen."
+L["Backdrop Settings"] = "Hintergruneinstellungen"
+L["Causes Masque to throw Lua errors whenever it encounters a problem with an add-on or skin."] = "Veranlasst Masque Lua-Fehler auszugeben, wann immer es ein Problem mit einem Addon oder einer Optik gibt."
+L["Checked"] = "Ausgewählt"
+L["Click this button to load Masque's options. You can also use the %s or %s chat command."] = "Klicken, um die Masque-Optionen zu laden. Du kannst dazu auch den Chatbefehl %s oder %s verwenden."
+L["Click to open Masque's options window."] = "Klicken, um Masques Optionensfenster zu öffnen."
+L["Color"] = "Farbe"
+L["Colors"] = "Farben"
+L["Cooldown"] = "Abklingzeit"
+L["Debug Mode"] = "Debugmodus"
+L["Disable"] = "Deaktivieren"
+L["Disable the skinning of this group."] = "Deaktiviert die Aussehensveränderung dieser Gruppe."
+L["Disabled"] = "Deaktiviert"
+L["Enable"] = "Aktivieren"
+L["Enable the Backdrop texture."] = "Aktiviert die Hintergrundtextur."
+L["Enable the Minimap icon."] = "Aktiviert das Minikartensymbol"
+L["Equipped"] = "Ausgerüstet"
+L["Flash"] = "Leuchten"
+L["General"] = "Allgemein"
+L["Global"] = "Global"
+L["Gloss Settings"] = "Glanzeinstellungen"
+L["Highlight"] = "Hervorheben"
+L["Load Masque Options"] = "Masque-Optionen laden"
+L["Loading Masque Options..."] = "Lädt Masque-Optionen..."
+L["Masque debug mode disabled."] = "Masque Debugmodus deaktiviert."
+L["Masque debug mode enabled."] = "Masque Debugmodus aktiviert."
+L["Masque is a dynamic button skinning add-on."] = "Masque ist ein Addon zur Anpassung der Buttonoptiken."
+L["Minimap Icon"] = "Minikartensymbol"
+L["Normal"] = "Normal"
+L["Opacity"] = "Transparenz"
+L["Profiles"] = "Profile"
+L["Pushed"] = "Gedrückt"
+L["Reset all skin options to the defaults."] = "Setzt alle Optikoptionen auf Standard zurück."
+L["Reset Skin"] = "Optik zurücksetzen"
+L["Set the color of the Backdrop texture."] = "Lege die Farbe der Hintergrundtextur fest"
+L["Set the color of the Checked texture."] = "Lege die Farbe der Markiert-Textur fest"
+L["Set the color of the Cooldown animation."] = "Lege die Farbe der Abklingzeit-Animation fest."
+L["Set the color of the Disabled texture."] = "Lege die Farbe der Deaktiviert-Textur fest"
+L["Set the color of the Equipped item texture."] = "Lege die Farbe der Ausgerüstetes-Item textur fest."
+L["Set the color of the Flash texture."] = "Lege die Farbe der Leucht-Textur fest"
+L["Set the color of the Gloss texture."] = "Lege die Farbe der Glanz-Textur fest"
+L["Set the color of the Highlight texture."] = "Lege die Farbe Hervorgehoben-Textur"
+L["Set the color of the Normal texture."] = "Lege die Farbe der Normal-Textur fest"
+L["Set the color of the Pushed texture."] = "Lege die Farbe der Gedrückt-Textur fest"
+L["Set the intensity of the Gloss color."] = "Lege die Intensität der Glanzfarbe fest."
+L["Set the skin for this group."] = "Lege die Optik dieser Gruppe fest"
+L["Skin"] = "Optik"
+L["This section will allow you to skin the buttons of the add-ons and add-on groups registered with Masque."] = "Dieser Abschnitt ermöglicht es dir, das Aussehen von Buttons der Addons und Addongruppen anzupassen, die mit Masque verbunden sind."
