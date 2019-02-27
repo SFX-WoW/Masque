ce5f3acd15edcf57f607faa68a68c68a33ff821b
StormFX <stormfx@gmail.com>
2019-02-27 10:36:18 -0600

    - Add LDB.lua.

---
+++ Options/LDB.lua
@@ -0,0 +1,59 @@
+--[[
+
+	This file is part of 'Masque', an add-on for World of Warcraft. For license information,
+	please see the included License.txt file.
+
+	* File...: Options\LDB.lua
+	* Author.: StormFX
+
+	LDB Launcher
+
+]]
+
+-- GLOBALS: LibStub
+
+local MASQUE, Core = ...
+
+----------------------------------------
+-- Setup
+----------------------------------------
+
+do
+	local Setup = Core.Setup
+
+	function Setup.LDB(self)
+		local LDB = LibStub("LibDataBroker-1.1", true)
+		if LDB then
+			local L = self.Locale
+
+			-- Create the LDB object.
+			self.LDBO = LDB:NewDataObject(MASQUE, {
+				type  = "launcher",
+				label = MASQUE,
+				icon  = "Interface\\Addons\\Masque\\Textures\\Icon",
+				OnClick = function(self, Button)
+					if Button == "LeftButton" or Button == "RightButton" then
+						Core:ToggleOptions()
+					end
+				end,
+				OnTooltipShow = function(Tip)
+					if not Tip or not Tip.AddLine then
+						return
+					end
+					Tip:AddLine(MASQUE)
+					Tip:AddLine(L["Click to open Masque's settings."], 1, 1, 1)
+				end,
+			})
+
+			-- Set up the icon.
+			local LDBI = LibStub("LibDBIcon-1.0", true)
+			if LDBI then
+				LDBI:Register(MASQUE, self.LDBO, self.db.profile.LDB)
+				self.LDBI = LDBI
+			end
+		end
+
+		-- GC
+		Setup.LDB = nil
+	end
+end
