2713b20fea73a80d93a42b15431292f431c237e1
StormFX <stormfx@gmail.com>
2019-02-27 17:46:36 -0600

    - Fix shadowing upvalue.

--- Options/LDB.lua
+++ Options/LDB.lua
@@ -31,7 +31,7 @@ do
 				type  = "launcher",
 				label = MASQUE,
 				icon  = "Interface\\Addons\\Masque\\Textures\\Icon",
-				OnClick = function(self, Button)
+				OnClick = function(Tip, Button)
 					if Button == "LeftButton" or Button == "RightButton" then
 						Core:ToggleOptions()
 					end
