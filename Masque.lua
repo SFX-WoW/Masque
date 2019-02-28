d5e8838fd3648230d7f415360606ab73449d6479
StormFX <stormfx@gmail.com>
2019-02-28 10:51:39 -0600

    - Update for options changes.

--- Masque.lua
+++ Masque.lua
@@ -51,26 +51,6 @@ do
 	}
 end
 
-local L = Core.Locale
-local LDBI = LibStub("LibDBIcon-1.0", true)
-
-----------------------------------------
--- Basic Options Table
----
-
-Core.Options = {
-	type = "group",
-	name = MASQUE,
-	args = {
-		General = {
-			type = "group",
-			name = L["General"],
-			order = 0,
-			args = {},
-		},
-	},
-}
-
 ----------------------------------------
 -- Add-On
 ---
@@ -84,6 +64,8 @@ do
 		local Defaults = {
 			profile = {
 				Debug = false,
+				SkinInfo = true,
+				StandAlone = false,
 				Groups = {
 					["*"] = {
 						Inherit = true,
@@ -101,68 +83,40 @@ do
 				},
 			},
 		}
+
 		local db = LibStub("AceDB-3.0"):New("MasqueDB", Defaults, true)
 		db.RegisterCallback(Core, "OnProfileChanged", "Update")
 		db.RegisterCallback(Core, "OnProfileCopied", "Update")
 		db.RegisterCallback(Core, "OnProfileReset", "Update")
 		Core.db = db
+
+		-- LibDualSpec-1.0
+		local LDS = LibStub("LibDualSpec-1.0", true)
+		if LDS then
+			LDS:EnhanceDatabase(Core.db, MASQUE)
+		end
+
+		-- Slash Commands
 		SLASH_MASQUE1 = "/msq"
 		SLASH_MASQUE2 = "/masque"
 		SlashCmdList["MASQUE"] = function(Cmd, ...)
 			if Cmd == "debug" then
-				Core:ToggleDebug()
+				Core.ToggleDebug()
 			else
-				Core:ShowOptions()
+				Core:ToggleOptions()
 			end
 		end
 	end
 
-	local ACR = LibStub("AceConfigRegistry-3.0")
-	local LDB = LibStub("LibDataBroker-1.1", true)
-
 	-- PLAYER_LOGIN Event
 	function Masque:OnEnable()
-		local db = Core.db.profile
-		ACR:RegisterOptionsTable(MASQUE, Core.Options)
-		Core.ACR = ACR
-		Core.OptionsPanel = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(MASQUE, MASQUE, nil, "General")
-		Core.Options.args.General.args.Load = {
-			type = "execute",
-			name = L["Load Masque Options"],
-			desc = (L["Click this button to load Masque's options. You can also use the %s or %s chat command."]):format("|cffffcc00/msq|r", "|cffffcc00/masque|r"),
-			func = function()
-				Core:LoadOptions()
-				InterfaceOptionsFrame_OpenToCategory(Core.OptionsPanel.Addons)
-			end,
-			hidden = function()
-				return Core.OptionsLoaded
-			end,
-			order = 0,
-		}
-		if LDB then
-			Core.LDBO = LDB:NewDataObject(MASQUE, {
-				type  = "launcher",
-				label = MASQUE,
-				icon  = "Interface\\Addons\\Masque\\Textures\\Icon",
-				OnClick = function(Tip, Button)
-					if Button == "LeftButton" or Button == "RightButton" then
-						Core:ShowOptions()
-					end
-				end,
-				OnTooltipShow = function(Tip)
-					if not Tip or not Tip.AddLine then
-						return
-					end
-					Tip:AddLine(MASQUE)
-					Tip:AddLine(L["Click to open Masque's options window."], 1, 1, 1)
-				end,
-			})
-			Core.LDB = LDB
-			if LDBI then
-				LDBI:Register(MASQUE, Core.LDBO, db.LDB)
-				Core.LDBI = LDBI
-			end
+		-- Core Options
+		local Setup = Core.Setup
+		if Setup then
+			Setup("Core")
+			Setup("LDB")
 		end
+
 		-- Re-skin queued groups.
 		if Core.Queue then
 			Core.Queue:ReSkin()
@@ -185,28 +139,38 @@ end
 ---
 
 do
+	local L = Core.Locale
+
 	-- Toggles debug mode.
-	function Core:ToggleDebug()
-		local db = self.db.profile
+	function Core.ToggleDebug()
+		local db = Core.db.profile
+
 		db.Debug = not db.Debug
 		Core.Debug = db.Debug
+
 		if db.Debug then
-			print("|cffffff99"..L["Masque debug mode disabled."].."|r")
-		else
 			print("|cffffff99"..L["Masque debug mode enabled."].."|r")
+		else
+			print("|cffffff99"..L["Masque debug mode disabled."].."|r")
 		end
 	end
 
 	-- Updates on profile activity.
 	function Core:Update()
 		-- Debug
-		Core.Debug = self.db.profile.Debug
+		self.Debug = self.db.profile.Debug
 
 		-- Skin Settings
-		local Global = Core:Group()
+		local Global = self.GetGroup()
 		Global:Update()
 
+		-- Skin Info Panel
+		if self.OptionsLoaded then
+			self.Setup("Info")
+		end
+
 		-- LDB Icon
+		local LDBI = LibStub("LibDBIcon-1.0", true)
 		if LDBI then
 			LDBI:Refresh(MASQUE, Core.db.profile.LDB)
 		end
