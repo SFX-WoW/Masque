bbc5939e9f7d27190bbdece4ed69c70aa2f6a94e
StormFX <stormfx@gmail.com>
2019-03-26 18:32:57 -0500

    - Use a method to retrieve utility functions.

--- Core/Utility.lua
+++ Core/Utility.lua
@@ -6,7 +6,7 @@
 	* File...: Core\Utility.lua
 	* Author.: StormFX
 
-	Utility Functions
+	Utility
 
 ]]
 
@@ -21,37 +21,66 @@ local _, Core = ...
 local type = type
 
 ----------------------------------------
--- Utility
+-- Functions
 ---
 
--- Returns a set of color values.
-function Core.GetColor(Color, Alpha)
-	if type(Color) == "table" then
-		return Color[1] or 1, Color[2] or 1, Color[3] or 1, Alpha or Color[4] or 1
-	else
-		return 1, 1, 1, Alpha or 1
+do
+	----------------------------------------
+	-- Size
+	---
+
+	-- Returns the height and width of a region.
+	local function GetSize(Width, Height, xScale, yScale)
+		local w = (Width or 36) * xScale
+		local h = (Height or 36) * yScale
+		return w, h
 	end
-end
 
--- Returns the x and y scale of a button.
-function Core.GetScale(Button)
-	local x = (Button:GetWidth() or 36) / 36
-	local y = (Button:GetHeight() or 36) / 36
-	return x, y
-end
+	----------------------------------------
+	-- Color
+	---
 
--- Returns the height and width of a region.
-function Core.GetSize(Width, Height, xScale, yScale)
-	local w = (Width or 36) * xScale
-	local h = (Height or 36) * yScale
-	return w, h
-end
+	-- Returns a set of color values.
+	local function GetColor(Color, Alpha)
+		if type(Color) == "table" then
+			return Color[1] or 1, Color[2] or 1, Color[3] or 1, Alpha or Color[4] or 1
+		else
+			return 1, 1, 1, Alpha or 1
+		end
+	end
 
--- Returns a set of texture coordinates.
-function Core.GetTexCoords(Coords)
-	if type(Coords) == "table" then
-		return Coords[1] or 0, Coords[2] or 1, Coords[3] or 0, Coords[4] or 1
-	else
-		return 0, 1, 0, 1
+	----------------------------------------
+	-- Coords
+	---
+
+	-- Returns a set of texture coordinates.
+	local function GetTexCoords(Coords)
+		if type(Coords) == "table" then
+			return Coords[1] or 0, Coords[2] or 1, Coords[3] or 0, Coords[4] or 1
+		else
+			return 0, 1, 0, 1
+		end
+	end
+
+	----------------------------------------
+	-- Core
+	---
+
+	-- Returns a list of utility functions.
+	function Core.Utility()
+		return GetSize, SetPoints, GetColor, GetTexCoords
+	end
+
+	----------------------------------------
+	-- Scale
+	-- @ Core\Button.lua
+	-- @ Core\Regions\Frame.lua
+	---
+
+	-- Returns the x and y scale of a button.
+	function Core.GetScale(Button)
+		local x = (Button:GetWidth() or 36) / 36
+		local y = (Button:GetHeight() or 36) / 36
+		return x, y
 	end
 end
