502e2cd560b3f79223ec8cd115a5cd0d85700f20
StormFX <stormfx@gmail.com>
2019-03-26 19:45:56 -0500

    - Move SpellAlert code to its own file.

--- Core/Button.lua
+++ Core/Button.lua
@@ -366,90 +366,6 @@ do
 end
 
 ----------------------------------------
--- Spell Alert
----
-
-local UpdateSpellAlert
-
-do
-	local Alerts = {
-		Square = {
-			Glow = "Interface\\SpellActivationOverlay\\IconAlert",
-			Ants = "Interface\\SpellActivationOverlay\\IconAlertAnts",
-		},
-		Circle = {
-			Glow = "Interface\\AddOns\\Masque\\Textures\\SpellAlert\\IconAlert-Circle",
-			Ants = "Interface\\AddOns\\Masque\\Textures\\SpellAlert\\IconAlertAnts-Circle",
-		},
-	}
-
-	-- Hook to update the spell alert animation.
-	function UpdateSpellAlert(Button)
-		local Overlay = Button.overlay
-		if (not Overlay) or (not Overlay.spark) then return end
-		if Overlay.__MSQ_Shape ~= Button.__MSQ_Shape then
-			local Shape = Button.__MSQ_Shape
-			local Glow, Ants
-			if Shape and Alerts[Shape] then
-				Glow = Alerts[Shape].Glow or Alerts.Square.Glow
-				Ants = Alerts[Shape].Ants or Alerts.Square.Ants
-			else
-				Glow = Alerts.Square.Glow
-				Ants = Alerts.Square.Ants
-			end
-			Overlay.innerGlow:SetTexture(Glow)
-			Overlay.innerGlowOver:SetTexture(Glow)
-			Overlay.outerGlow:SetTexture(Glow)
-			Overlay.outerGlowOver:SetTexture(Glow)
-			Overlay.spark:SetTexture(Glow)
-			Overlay.ants:SetTexture(Ants)
-			Overlay.__MSQ_Shape = Button.__MSQ_Shape
-		end
-	end
-	hooksecurefunc("ActionButton_ShowOverlayGlow", UpdateSpellAlert)
-
-	-- API: Allows add-ons to call the update when not using the native API.
-	function Core.API:UpdateSpellAlert(Button)
-		if type(Button) ~= "table" then
-			return
-		end
-		UpdateSpellAlert(Button)
-	end
-
-	-- API: Adds a spell alert texture set.
-	function Core.API:AddSpellAlert(Shape, Glow, Ants)
-		if type(Shape) ~= "string" then
-			if Core.Debug then
-				error("Bad argument to method 'AddSpellAlert'. 'Shape' must be a string.", 2)
-			end
-			return
-		end
-		local Overlay = Alerts[Shape] or {}
-		if type(Glow) == "string" then
-			Overlay.Glow = Glow
-		end
-		if type(Ants) == "string" then
-			Overlay.Ants = Ants
-		end
-		Alerts[Shape] = Overlay
-	end
-
-	-- API: Returns a spell alert texture set.
-	function Core.API:GetSpellAlert(Shape)
-		if type(Shape) ~= "string" then
-			if Core.Debug then
-				error("Bad argument to method 'GetSpellAlert'. 'Shape' must be a string.", 2)
-			end
-			return
-		end
-		local Overlay = Alerts[Shape]
-		if Overlay then
-			return Overlay.Glow, Overlay.Ants
-		end
-	end
-end
-
-----------------------------------------
 -- Button Skinning Function
 ---
 
@@ -459,6 +375,13 @@ do
 	-- Applies a skin to a button and its associated layers.
 	function Core.SkinButton(Button, ButtonData, SkinID, Backdrop, Shadow, Gloss, Colors, IsActionButton)
 		if not Button then return end
+
+		----------------------------------------
+		-- Set Up
+		---
+
+		-- Button Type
+		local bType = Button.__MSQ_bType
 
 		-- Skin
 		local Skin = (SkinID and Skins[SkinID]) or Skins["Classic"]
@@ -567,9 +490,10 @@ do
 			SkinRegion("Frame", Shine, Button, Skin.AutoCastShine, xScale, yScale)
 		end
 
-		-- Spell Alert
-		if Button:GetObjectType() == "CheckButton" then
-			UpdateSpellAlert(Button)
-		end
+		----------------------------------------
+		-- SpellAlert
+		---
+
+		SkinRegion("SpellAlert", Button)
 	end
 end
