c37af74def45b37a73c1b92c69bb97a2fa1bb544
StormFX <stormfx@gmail.com>
2019-04-02 13:00:24 -0500

    - Code clean-up.

--- Core/Button.lua
+++ Core/Button.lua
@@ -30,167 +30,159 @@ local GetScale = Core.GetScale
 -- @ Skins\Skins
 local Skins, __Empty = Core.Skins, Core.__Empty
 
+-- @ Skins\Regions
+local RegList = Core.RegList
+
+-- @ Core\Core
+local SkinRegion = Core.SkinRegion
+
 ----------------------------------------
--- Button
+-- Shape
 ---
 
-do
-	-- @ Skins\Regions
-	local RegList = Core.RegList
+-- List of valid shapes.
+local Shapes = {
+	Circle = "Circle",
+	Square = "Square",
+}
 
-	-- List of valid shapes.
-	local Shapes = {
-		Circle = "Circle",
-		Square = "Square",
-	}
+-- Validates and returns a shape.
+local function GetShape(Shape)
+	return Shape and Shapes[Shape] or "Square"
+end
 
-	-- Validates and returns a shape.
-	local function GetShape(Shape)
-		return Shape and Shapes[Shape] or "Square"
+----------------------------------------
+-- Button-Skinning Function
+---
+
+-- Applies a skin to a button and its associated layers.
+function Core.SkinButton(Button, Regions, SkinID, Backdrop, Shadow, Gloss, Colors)
+	if not Button then return end
+
+	----------------------------------------
+	-- Set Up
+	---
+
+	local bType = Button.__MSQ_bType
+
+	local Skin = (SkinID and Skins[SkinID]) or Skins["Classic"]
+
+	if type(Colors) ~= "table" then
+		Colors = __Empty
+	end
+
+	local xScale, yScale = GetScale(Button)
+
+	Button.__MSQ_Shape = GetShape(Skin.Shape)
+
+	----------------------------------------
+	-- Backdrop
+	---
+
+	Button.FloatingBG = Button.FloatingBG or Regions.Backdrop
+
+	SkinRegion("Backdrop", Backdrop, Button, Skin.Backdrop, Colors.Backdrop, xScale, yScale)
+
+	----------------------------------------
+	-- Icon
+	---
+
+	local Icon = Regions.Icon
+
+	if Icon then
+		SkinRegion("Icon", Icon, Button, Skin.Icon, xScale, yScale)
 	end
 
 	----------------------------------------
-	-- Button-Skinning Function
+	-- Shadow
 	---
 
-	-- @ Core\Core
-	local SkinRegion = Core.SkinRegion
+	SkinRegion("Shadow", Shadow, Button, Skin.Shadow, Colors.Shadow, xScale, yScale)
 
-	-- Applies a skin to a button and its associated layers.
-	function Core.SkinButton(Button, Regions, SkinID, Backdrop, Shadow, Gloss, Colors)
-		if not Button then return end
+	----------------------------------------
+	-- Normal
+	---
 
-		----------------------------------------
-		-- Set Up
-		---
+	local Normal = Regions.Normal
 
-		-- Button Type
-		local bType = Button.__MSQ_bType
+	if Normal ~= false then
+		SkinRegion("Normal", Normal, Button, Skin.Normal, Colors.Normal, xScale, yScale)
+	end
 
-		-- Skin
-		local Skin = (SkinID and Skins[SkinID]) or Skins["Classic"]
+	----------------------------------------
+	-- Text/Texture
+	---
 
-		-- Color
-		if type(Colors) ~= "table" then
-			Colors = __Empty
-		end
+	local Layers = RegList[bType]
 
-		-- Scale
-		local xScale, yScale = GetScale(Button)
+	-- Iterate the regions for this button type.
+	for Layer, Info in pairs(Layers) do
+		local nType = Info.nType
 
-		-- Shape
-		Button.__MSQ_Shape = GetShape(Skin.Shape)
+		if nType then
+			local Region = Regions[Layer]
 
-		----------------------------------------
-		-- Backdrop
-		---
-
-		Button.FloatingBG = Button.FloatingBG or Regions.Backdrop
-
-		SkinRegion("Backdrop", Backdrop, Button, Skin.Backdrop, Colors.Backdrop, xScale, yScale)
-
-		----------------------------------------
-		-- Icon
-		---
-
-		local Icon = Regions.Icon
-
-		if Icon then
-			SkinRegion("Icon", Icon, Button, Skin.Icon, xScale, yScale)
-		end
-
-		----------------------------------------
-		-- Shadow
-		---
-
-		SkinRegion("Shadow", Shadow, Button, Skin.Shadow, Colors.Shadow, xScale, yScale)
-
-		----------------------------------------
-		-- Normal
-		---
-
-		local Normal = Regions.Normal
-
-		if Normal ~= false then
-			SkinRegion("Normal", Normal, Button, Skin.Normal, Colors.Normal, xScale, yScale)
-		end
-
-		----------------------------------------
-		-- Other Regions
-		---
-
-		local Layers = RegList[bType]
-
-		-- Iterate the regions for this button type.
-		for Layer, Info in pairs(Layers) do
-			local nType = Info.nType
-
-			-- Only process regions with an internal type.
-			if nType then
-				local Region = Regions[Layer]
-
-				-- Skin the region, if available.
-				if Region then
-					if nType == "Text" then
-						SkinRegion("Text", Region, Button, Layer, Skin[Layer], xScale, yScale)
-					else
-						SkinRegion(nType, Region, Button, Layer, Skin[Layer], Colors[Layer], xScale, yScale)
-					end
+			if Region then
+				if nType == "Text" then
+					SkinRegion("Text", Region, Button, Layer, Skin[Layer], xScale, yScale)
+				else
+					SkinRegion(nType, Region, Button, Layer, Skin[Layer], Colors[Layer], xScale, yScale)
 				end
 			end
 		end
-
-		----------------------------------------
-		-- Gloss
-		---
-
-		SkinRegion("Gloss", Gloss, Button, Skin.Gloss, Colors.Gloss, xScale, yScale)
-
-		----------------------------------------
-		-- Cooldown
-		---
-
-		local Cooldown = Regions.Cooldown
-
-		if Cooldown then
-			SkinRegion("Cooldown", Cooldown, Button, Skin.Cooldown, Colors.Cooldown, xScale, yScale)
-		end
-
-		----------------------------------------
-		-- Action Button Only
-		---
-
-		if bType ~= "Action" then return end
-
-		----------------------------------------
-		-- ChargeCooldown
-		---
-
-		local Charge = Button.chargeCooldown
-
-		-- Set this so it can be accessed later.
-		local ChargeSkin = Skin.ChargeCooldown
-		Button.__MSQ_ChargeSkin = ChargeSkin
-
-		if Charge then
-			SkinRegion("Cooldown", Charge, Button, ChargeSkin, nil, xScale, yScale)
-		end
-
-		----------------------------------------
-		-- AutoCastShine
-		---
-
-		local Shine = Regions.AutoCastShine
-
-		if Shine then
-			Button.__MSQ_Shine = Shine
-			SkinRegion("Frame", Shine, Button, Skin.AutoCastShine, xScale, yScale)
-		end
-
-		----------------------------------------
-		-- SpellAlert
-		---
-
-		SkinRegion("SpellAlert", Button)
 	end
+
+	----------------------------------------
+	-- Gloss
+	---
+
+	SkinRegion("Gloss", Gloss, Button, Skin.Gloss, Colors.Gloss, xScale, yScale)
+
+	----------------------------------------
+	-- Cooldown
+	---
+
+	local Cooldown = Regions.Cooldown
+
+	if Cooldown then
+		SkinRegion("Cooldown", Cooldown, Button, Skin.Cooldown, Colors.Cooldown, xScale, yScale)
+	end
+
+	----------------------------------------
+	-- Action Button Only
+	---
+
+	if bType ~= "Action" then return end
+
+	----------------------------------------
+	-- ChargeCooldown
+	---
+
+	local Charge = Button.chargeCooldown
+
+	-- Set this so it can be accessed later.
+	local ChargeSkin = Skin.ChargeCooldown
+
+	Button.__MSQ_ChargeSkin = ChargeSkin
+
+	if Charge then
+		SkinRegion("Cooldown", Charge, Button, ChargeSkin, nil, xScale, yScale)
+	end
+
+	----------------------------------------
+	-- AutoCastShine
+	---
+
+	local Shine = Regions.AutoCastShine
+
+	if Shine then
+		Button.__MSQ_Shine = Shine
+		SkinRegion("Frame", Shine, Button, Skin.AutoCastShine, xScale, yScale)
+	end
+
+	----------------------------------------
+	-- SpellAlert
+	---
+
+	SkinRegion("SpellAlert", Button)
 end
