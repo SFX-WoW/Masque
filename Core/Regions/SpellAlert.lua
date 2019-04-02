4ac47ec28af0d900959ee75ff40a653a42d74295
StormFX <stormfx@gmail.com>
2019-04-02 13:55:07 -0500

    - Code clean-up.

--- Core/Regions/SpellAlert.lua
+++ Core/Regions/SpellAlert.lua
@@ -12,7 +12,7 @@
 
 ]]
 
--- GLOBALS:
+-- GLOBALS: hooksecurefunc
 
 local _, Core = ...
 
@@ -22,128 +22,112 @@ local _, Core = ...
 
 local error, type = error, type
 
+local Alerts = {
+	Square = {
+		Glow = [[Interface\SpellActivationOverlay\IconAlert]],
+		Ants = [[Interface\SpellActivationOverlay\IconAlertAnts]],
+	},
+	Circle = {
+		Glow = [[Interface\AddOns\Masque\\Textures\SpellAlert\IconAlert-Circle]],
+		Ants = [[Interface\AddOns\Masque\Textures\SpellAlert\IconAlertAnts-Circle]],
+	},
+}
+
 ----------------------------------------
--- SpellAlert
+-- Update Function
 ---
 
-do
-	local Alerts = {
-		Square = {
-			Glow = [[Interface\SpellActivationOverlay\IconAlert]],
-			Ants = [[Interface\SpellActivationOverlay\IconAlertAnts]],
-		},
-		Circle = {
-			Glow = [[Interface\AddOns\Masque\\Textures\SpellAlert\IconAlert-Circle]],
-			Ants = [[Interface\AddOns\Masque\Textures\SpellAlert\IconAlertAnts-Circle]],
-		},
-	}
+-- Hook to update the 'SpellAlert' animation.
+local function UpdateSpellAlert(Button)
+	local Overlay = Button.overlay
 
-	----------------------------------------
-	-- Update Function
-	---
-
-	-- Hook to update the 'SpellAlert' animation.
-	local function UpdateSpellAlert(Button)
-		local Overlay = Button.overlay
-
-		-- Exit if there's no region.
-		if not Overlay or not Overlay.spark then
-			return
-		end
-
-		local Shape = Button.__MSQ_Shape
-
-		-- Compare the shapes.
-		if Overlay.__MSQ_Shape ~= Shape then
-			local Glow, Ants
-
-			-- Shape-Specific Textures
-			if Shape and Alerts[Shape] then
-				Glow = Alerts[Shape].Glow or Alerts.Square.Glow
-				Ants = Alerts[Shape].Ants or Alerts.Square.Ants
-
-			-- Default
-			else
-				Glow = Alerts.Square.Glow
-				Ants = Alerts.Square.Ants
-			end
-
-			-- Set the textures.
-			Overlay.innerGlow:SetTexture(Glow)
-			Overlay.innerGlowOver:SetTexture(Glow)
-			Overlay.outerGlow:SetTexture(Glow)
-			Overlay.outerGlowOver:SetTexture(Glow)
-			Overlay.spark:SetTexture(Glow)
-			Overlay.ants:SetTexture(Ants)
-
-			-- Set the shape.
-			Overlay.__MSQ_Shape = Shape
-		end
+	if not Overlay or not Overlay.spark then
+		return
 	end
 
-	hooksecurefunc("ActionButton_ShowOverlayGlow", UpdateSpellAlert)
+	local Shape = Button.__MSQ_Shape
 
-	----------------------------------------
-	-- Region-Skinning Function
-	---
+	if Overlay.__MSQ_Shape ~= Shape then
+		local Glow, Ants
 
-	Core.SkinRegion.SpellAlert = UpdateSpellAlert
+		if Shape and Alerts[Shape] then
+			Glow = Alerts[Shape].Glow or Alerts.Square.Glow
+			Ants = Alerts[Shape].Ants or Alerts.Square.Ants
 
-	----------------------------------------
-	-- API
-	---
-
-	local API = Core.API
-
-	-- Wrapper for the Update function.
-	-- * Allows add-ons to call the function when not using the native API.
-	function API:UpdateSpellAlert(Button)
-		if type(Button) ~= "table" then
-			return
+		else
+			Glow = Alerts.Square.Glow
+			Ants = Alerts.Square.Ants
 		end
 
-		UpdateSpellAlert(Button)
+		Overlay.innerGlow:SetTexture(Glow)
+		Overlay.innerGlowOver:SetTexture(Glow)
+		Overlay.outerGlow:SetTexture(Glow)
+		Overlay.outerGlowOver:SetTexture(Glow)
+		Overlay.spark:SetTexture(Glow)
+		Overlay.ants:SetTexture(Ants)
+
+		Overlay.__MSQ_Shape = Shape
+	end
+end
+
+hooksecurefunc("ActionButton_ShowOverlayGlow", UpdateSpellAlert)
+
+----------------------------------------
+-- Region-Skinning Function
+---
+
+Core.SkinRegion.SpellAlert = UpdateSpellAlert
+
+----------------------------------------
+-- API
+---
+
+local API = Core.API
+
+-- Wrapper for the Update function.
+-- * Allows add-ons to call the function when not using the native API.
+function API:UpdateSpellAlert(Button)
+	if type(Button) ~= "table" then
+		return
 	end
 
-	-- Adds or overwrites a 'SpellAlert' texture set.
-	function API:AddSpellAlert(Shape, Glow, Ants)
-		if type(Shape) ~= "string" then
-			if Core.Debug then
-				error("Bad argument to API method 'AddSpellAlert'. 'Shape' must be a string.", 2)
-			end
-			return
+	UpdateSpellAlert(Button)
+end
+
+-- Adds or overwrites a 'SpellAlert' texture set.
+function API:AddSpellAlert(Shape, Glow, Ants)
+	if type(Shape) ~= "string" then
+		if Core.Debug then
+			error("Bad argument to API method 'AddSpellAlert'. 'Shape' must be a string.", 2)
 		end
-
-		-- Get or create the shape table.
-		local Overlay = Alerts[Shape] or {}
-
-		-- Add the 'Glow' texture.
-		if type(Glow) == "string" then
-			Overlay.Glow = Glow
-		end
-
-		-- Add the 'Ants' texture.
-		if type(Ants) == "string" then
-			Overlay.Ants = Ants
-		end
-
-		Alerts[Shape] = Overlay
+		return
 	end
 
-	-- Retrieves a 'SpellAlert' texture set.
-	function API:GetSpellAlert(Shape)
-		if type(Shape) ~= "string" then
-			if Core.Debug then
-				error("Bad argument to API method 'GetSpellAlert'. 'Shape' must be a string.", 2)
-			end
-			return
-		end
+	local Overlay = Alerts[Shape] or {}
 
-		local Overlay = Alerts[Shape]
+	if type(Glow) == "string" then
+		Overlay.Glow = Glow
+	end
 
-		-- Return the texture list.
-		if Overlay then
-			return Overlay.Glow, Overlay.Ants
+	if type(Ants) == "string" then
+		Overlay.Ants = Ants
+	end
+
+	Alerts[Shape] = Overlay
+end
+
+-- Retrieves a 'SpellAlert' texture set.
+function API:GetSpellAlert(Shape)
+	if type(Shape) ~= "string" then
+		if Core.Debug then
+			error("Bad argument to API method 'GetSpellAlert'. 'Shape' must be a string.", 2)
 		end
+		return
+	end
+
+	local Overlay = Alerts[Shape]
+
+	if Overlay then
+		return Overlay.Glow, Overlay.Ants
 	end
 end
