912d8f3875c1fca4cc8b6ef980e009bb863a3574
StormFX <stormfx@gmail.com>
2019-04-02 13:05:31 -0500

    - Code clean-up.

--- Core/Core.lua
+++ Core/Core.lua
@@ -21,14 +21,10 @@ local _, Core = ...
 local _G, setmetatable, type = _G, setmetatable, type
 
 ----------------------------------------
--- Core
+-- Type Validation
 ---
 
 do
-	----------------------------------------
-	-- Type Validation
-	---
-
 	-- Valid Object Types
 	local oTypes = {
 		Button = true,
@@ -88,93 +84,93 @@ do
 
 		return Type
 	end
+end
 
-	----------------------------------------
-	-- Region Retrieval
-	---
+----------------------------------------
+-- Region Retrieval
+---
 
-	-- Gets a button region.
-	function Core.GetRegion(Button, Info)
-		local Key, Region = Info.Key, nil
+-- Gets a button region.
+function Core.GetRegion(Button, Info)
+	local Key, Region = Info.Key, nil
 
-		-- Check for a field first.
-		if Key then
-			local Value = Key and Button[Key]
+	-- Check for a field first.
+	if Key then
+		local Value = Key and Button[Key]
 
-			-- Make sure it points to a valid region.
-			if Value and type(Value) == "table" then
-				local Type = Value.GetObjectType and Value:GetObjectType()
+		-- Make sure it points to a valid region.
+		if Value and type(Value) == "table" then
+			local Type = Value.GetObjectType and Value:GetObjectType()
 
-				-- Compare the types.
-				if Type == Info.Type then
-					Region = Value
-				end
+			-- Compare the types.
+			if Type == Info.Type then
+				Region = Value
 			end
 		end
-
-		-- Check for a method or global field if the field wasn't valid.
-		if not Region then
-			local Func, Name = Info.Func, Info.Name
-
-			-- Method Check
-			if Func then
-				local f = Func and Button[Func]
-				Region = f and f(Button)
-
-			-- Global Check
-			elseif Name then
-				local n = Button.GetName and Button:GetName()
-				Region = n and _G[n..Name]
-			end
-		end
-
-		return Region
 	end
 
-	----------------------------------------
-	-- Region Skinning
-	---
+	-- Check for a method or global field if the field wasn't valid.
+	if not Region then
+		local Func, Name = Info.Func, Info.Name
 
-	-- Metatable to call region-skinning functions.
-	Core.SkinRegion = setmetatable({}, {
-		__call = function(self, Region, ...)
-			local func = Region and self[Region]
-			if func then
-				func(...)
-			end
-		end,
-	})
+		-- Method Check
+		if Func then
+			local f = Func and Button[Func]
+			Region = f and f(Button)
 
-	----------------------------------------
-	-- Group Queue
-	---
+		-- Global Check
+		elseif Name then
+			local n = Button.GetName and Button:GetName()
+			Region = n and _G[n..Name]
+		end
+	end
 
-	-- Self-destructing table to skin groups created prior to PLAYER_LOGIN.
-	Core.Queue = {
-		-- Storage
-		Cache = {},
-
-		-- Adds a group to the queue.
-		Add = function(self, obj)
-			self.Cache[#self.Cache + 1] = obj
-			obj.Queued = true
-		end,
-
-		-- Re-Skins all queued groups.
-		ReSkin = function(self)
-			for i = 1, #self.Cache do
-				local obj = self.Cache[i]
-
-				-- Skin the group.
-				obj:ReSkin(true)
-				obj.Queued = nil
-			end
-
-			-- GC
-			self.Cache = nil
-			Core.Queue = nil
-		end,
-	}
-
-	setmetatable(Core.Queue, {__call = Core.Queue.Add})
+	return Region
 end
+
+----------------------------------------
+-- Region Skinning
+---
+
+-- Metatable to call region-skinning functions.
+Core.SkinRegion = setmetatable({}, {
+	__call = function(self, Region, ...)
+		local func = Region and self[Region]
+		if func then
+			func(...)
+		end
+	end,
+})
+
+----------------------------------------
+-- Group Queue
+---
+
+-- Self-destructing table to skin groups created prior to PLAYER_LOGIN.
+Core.Queue = {
+	-- Storage
+	Cache = {},
+
+	-- Adds a group to the queue.
+	Add = function(self, obj)
+		self.Cache[#self.Cache + 1] = obj
+		obj.Queued = true
+	end,
+
+	-- Re-Skins all queued groups.
+	ReSkin = function(self)
+		for i = 1, #self.Cache do
+			local obj = self.Cache[i]
+
+			-- Skin the group.
+			obj:ReSkin(true)
+			obj.Queued = nil
+		end
+
+		-- GC
+		self.Cache = nil
+		Core.Queue = nil
+	end,
+}
+
+setmetatable(Core.Queue, {__call = Core.Queue.Add})
