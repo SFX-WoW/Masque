81ec7b0598a894f32602c992202e22bda5c886f8
StormFX <stormfx@gmail.com>
2019-04-02 19:01:27 -0500

    - Code clean-up.

--- Core/Core.lua
+++ Core/Core.lua
@@ -21,104 +21,91 @@ local _, Core = ...
 local _G, setmetatable, type = _G, setmetatable, type
 
 ----------------------------------------
--- Type Validation
+-- Locals
 ---
 
-do
-	-- Valid Object Types
-	local oTypes = {
-		Button = true,
-		CheckButton = true,
-		Frame = true,
-	}
+-- Valid Types
+local oTypes = {
+	Button = true,
+	CheckButton = true,
+	Frame = true,
+}
 
-	-- Returns a button's object or internal type.
-	function Core.GetType(Button, oType)
-		local Type
+----------------------------------------
+-- Type Validator
+---
 
-		-- Object Type
-		if oType then
-			if type(Button) == "table" then
-				Type = Button.GetObjectType and Button:GetObjectType()
+-- Returns a button's object or internal type.
+function Core.GetType(Button, oType)
+	local Type
 
-				-- Only allow the certain object types.
-				if not Type or not oTypes[Type] then
-					Type = nil
-				end
-			end
+	if oType then
+		if type(Button) == "table" then
+			Type = Button.GetObjectType and Button:GetObjectType()
 
-			Button.__MSQ_oType = Type
-
-		-- Internal Type
-		else
-			oType = Button.__MSQ_oType
-			Type = "Button"
-
-			-- CheckButton
-			if oType == "CheckButton" then
-				-- Action Button
-				if Button.HotKey then
-					Type = "Action"
-				end
-
-			-- Button
-			elseif oType == "Button" then
-				-- Item Button
-				if Button.IconBorder then
-					Type = "Item"
-
-				-- Aura Button
-				elseif Button.duration then
-					Type = "Aura"
-
-					local bName = Button.GetName and Button:GetName()
-					local Border = bName and _G[bName.."Border"]
-
-					-- Debuff/Enchant Button
-					if Border then
-						Type = (Button.symbol and "Debuff") or "Enchant"
-					end
-				end
+			if not Type or not oTypes[Type] then
+				Type = nil
 			end
 		end
 
-		return Type
+		Button.__MSQ_oType = Type
+
+	else
+		oType = Button.__MSQ_oType
+		Type = "Button"
+
+		if oType == "CheckButton" then
+			if Button.HotKey then
+				Type = "Action"
+			end
+
+		elseif oType == "Button" then
+			if Button.IconBorder then
+				Type = "Item"
+
+			elseif Button.duration then
+				Type = "Aura"
+
+				local bName = Button.GetName and Button:GetName()
+				local Border = bName and _G[bName.."Border"]
+
+				if Border then
+					Type = (Button.symbol and "Debuff") or "Enchant"
+				end
+			end
+		end
 	end
+
+	return Type
 end
 
 ----------------------------------------
--- Region Retrieval
+-- Region Retriever
 ---
 
 -- Gets a button region.
 function Core.GetRegion(Button, Info)
 	local Key, Region = Info.Key, nil
 
-	-- Check for a field first.
 	if Key then
 		local Value = Key and Button[Key]
 
-		-- Make sure it points to a valid region.
 		if Value and type(Value) == "table" then
 			local Type = Value.GetObjectType and Value:GetObjectType()
 
-			-- Compare the types.
 			if Type == Info.Type then
 				Region = Value
 			end
 		end
 	end
 
-	-- Check for a method or global field if the field wasn't valid.
 	if not Region then
 		local Func, Name = Info.Func, Info.Name
 
-		-- Method Check
 		if Func then
 			local f = Func and Button[Func]
 			Region = f and f(Button)
 
-		-- Global Check
 		elseif Name then
 			local n = Button.GetName and Button:GetName()
 			Region = n and _G[n..Name]
@@ -129,7 +116,7 @@ function Core.GetRegion(Button, Info)
 end
 
 ----------------------------------------
--- Region Skinning
+-- Region Skinner
 ---
 
 -- Metatable to call region-skinning functions.
@@ -148,7 +135,6 @@ Core.SkinRegion = setmetatable({}, {
 
 -- Self-destructing table to skin groups created prior to PLAYER_LOGIN.
 Core.Queue = {
-	-- Storage
 	Cache = {},
 
 	-- Adds a group to the queue.
@@ -162,7 +148,6 @@ Core.Queue = {
 		for i = 1, #self.Cache do
 			local obj = self.Cache[i]
 
-			-- Skin the group.
 			obj:ReSkin(true)
 			obj.Queued = nil
 		end
