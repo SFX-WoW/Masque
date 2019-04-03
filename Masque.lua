9bac675e4f364cb8afcd9c496671c389a72e9eb2
StormFX <stormfx@gmail.com>
2019-04-02 20:17:20 -0500

    - Code clean-up.

--- Masque.lua
+++ Masque.lua
@@ -23,6 +23,15 @@ assert(LibStub, MASQUE.." requires LibStub.")
 local print = print
 
 ----------------------------------------
+-- Locals
+---
+
+local Masque = LibStub("AceAddon-3.0"):NewAddon(MASQUE)
+
+-- @ Locales\enUS
+local L = Core.Locale
+
+----------------------------------------
 -- API
 ---
 
@@ -55,123 +64,106 @@ end
 -- Add-On
 ---
 
-do
-	local Masque = LibStub("AceAddon-3.0"):NewAddon(MASQUE)
-
-	-- ADDON_LOADED Event
-	function Masque:OnInitialize()
-		-- DB Setup
-		local Defaults = {
-			profile = {
-				Debug = false,
-				SkinInfo = true,
-				StandAlone = false,
-				Groups = {
-					["*"] = {
-						Inherit = true,
-						Disabled = false,
-						SkinID = "Classic",
-						Backdrop = false,
-						Shadow = false,
-						Gloss = false,
-						Colors = {},
-					},
-				},
-				LDB = {
-					hide = true,
-					minimapPos = 220,
-					radius = 80,
+-- ADDON_LOADED Event
+function Masque:OnInitialize()
+	local Defaults = {
+		profile = {
+			Debug = false,
+			SkinInfo = true,
+			StandAlone = false,
+			Groups = {
+				["*"] = {
+					Inherit = true,
+					Disabled = false,
+					SkinID = "Classic",
+					Backdrop = false,
+					Shadow = false,
+					Gloss = false,
+					Colors = {},
 				},
 			},
-		}
+			LDB = {
+				hide = true,
+				minimapPos = 220,
+				radius = 80,
+			},
+		},
+	}
 
-		local db = LibStub("AceDB-3.0"):New("MasqueDB", Defaults, true)
-		db.RegisterCallback(Core, "OnProfileChanged", "Update")
-		db.RegisterCallback(Core, "OnProfileCopied", "Update")
-		db.RegisterCallback(Core, "OnProfileReset", "Update")
-		Core.db = db
+	local db = LibStub("AceDB-3.0"):New("MasqueDB", Defaults, true)
+	db.RegisterCallback(Core, "OnProfileChanged", "Update")
+	db.RegisterCallback(Core, "OnProfileCopied", "Update")
+	db.RegisterCallback(Core, "OnProfileReset", "Update")
+	Core.db = db
 
-		-- LibDualSpec-1.0
-		local LDS = LibStub("LibDualSpec-1.0", true)
-		if LDS then
-			LDS:EnhanceDatabase(Core.db, MASQUE)
-		end
-
-		-- Slash Commands
-		SLASH_MASQUE1 = "/msq"
-		SLASH_MASQUE2 = "/masque"
-		SlashCmdList["MASQUE"] = function(Cmd, ...)
-			if Cmd == "debug" then
-				Core.ToggleDebug()
-			else
-				Core:ToggleOptions()
-			end
-		end
+	local LDS = LibStub("LibDualSpec-1.0", true)
+	if LDS then
+		LDS:EnhanceDatabase(Core.db, MASQUE)
 	end
 
-	-- PLAYER_LOGIN Event
-	function Masque:OnEnable()
-		-- Core Options
-		local Setup = Core.Setup
-		if Setup then
-			Setup("Core")
-			Setup("LDB")
-		end
-
-		-- Re-skin queued groups.
-		if Core.Queue then
-			Core.Queue:ReSkin()
+	SLASH_MASQUE1 = "/msq"
+	SLASH_MASQUE2 = "/masque"
+	SlashCmdList["MASQUE"] = function(Cmd, ...)
+		if Cmd == "debug" then
+			Core.ToggleDebug()
+		else
+			Core:ToggleOptions()
 		end
 	end
+end
 
-	-- Wrapper for the DB:CopyProfile method.
-	function Masque:CopyProfile(Name, Silent)
-		Core.db:CopyProfile(Name, Silent)
+-- PLAYER_LOGIN Event
+function Masque:OnEnable()
+	local Setup = Core.Setup
+	if Setup then
+		Setup("Core")
+		Setup("LDB")
 	end
 
-	-- Wrapper for the DB:SetProfile method.
-	function Masque:SetProfile(Name)
-		Core.db:SetProfile(Name)
+	if Core.Queue then
+		Core.Queue:ReSkin()
 	end
+end
+
+-- Wrapper for the DB:CopyProfile method.
+function Masque:CopyProfile(Name, Silent)
+	Core.db:CopyProfile(Name, Silent)
+end
+
+-- Wrapper for the DB:SetProfile method.
+function Masque:SetProfile(Name)
+	Core.db:SetProfile(Name)
 end
 
 ----------------------------------------
 -- Core
 ---
 
-do
-	local L = Core.Locale
+-- Toggles debug mode.
+function Core.ToggleDebug()
+	local db = Core.db.profile
 
-	-- Toggles debug mode.
-	function Core.ToggleDebug()
-		local db = Core.db.profile
+	db.Debug = not db.Debug
+	Core.Debug = db.Debug
 
-		db.Debug = not db.Debug
-		Core.Debug = db.Debug
-
-		if db.Debug then
-			print("|cffffff99"..L["Masque debug mode enabled."].."|r")
-		else
-			print("|cffffff99"..L["Masque debug mode disabled."].."|r")
-		end
+	if db.Debug then
+		print("|cffffff99"..L["Masque debug mode enabled."].."|r")
+	else
+		print("|cffffff99"..L["Masque debug mode disabled."].."|r")
 	end
+end
 
-	-- Updates on profile activity.
-	function Core:Update()
-		-- Debug
-		self.Debug = self.db.profile.Debug
+-- Updates on profile activity.
+function Core:Update()
+	self.Debug = self.db.profile.Debug
 
-		-- Skin Settings
-		local Global = self.GetGroup()
-		Global:Update()
+	local Global = self.GetGroup()
+	Global:__Update()
 
-		-- Skin Info Panel
-		self.Setup("Info")
+	self.Setup("Info")
 
-		-- LDB Icon
-		local LDBI = LibStub("LibDBIcon-1.0", true)
-		if LDBI then
-			LDBI:Refresh(MASQUE, Core.db.profile.LDB)
-		end
+	local LDBI = LibStub("LibDBIcon-1.0", true)
+	if LDBI then
+		LDBI:Refresh(MASQUE, Core.db.profile.LDB)
 	end
 end
