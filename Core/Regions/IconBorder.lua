b3ddb264eb56724ac3857a8beef87483d3a8dd17
StormFX <stormfx@gmail.com>
2019-04-30 12:12:23 -0500

    - Update defaults.

--- Core/Regions/IconBorder.lua
+++ Core/Regions/IconBorder.lua
@@ -8,7 +8,7 @@
 
 	Texture Regions
 
-	* See Skins\Regions.lua for region defaults.
+	* See Skins\Default.lua for region defaults.
 
 ]]
 
@@ -17,14 +17,17 @@
 local _, Core = ...
 
 ----------------------------------------
--- API
+-- WoW API
 ---
 
 local hooksecurefunc = hooksecurefunc
 
 ----------------------------------------
--- Locals
+-- Internal
 ---
+
+-- @ Skins\Default
+local Default = Core.Skins.Default.IconBorder
 
 -- @ Core\Utility
 local GetSize, SetPoints = Core.GetSize, Core.SetPoints
@@ -34,12 +37,19 @@ local GetTexCoords = Core.GetTexCoords
 local SkinRegion = Core.SkinRegion
 
 ----------------------------------------
+-- Locals
+---
+
+local DEF_TEXTURE = Default.Texture
+local DEF_RELIC_TEXTURE = Default.RelicTexture
+
+----------------------------------------
 -- Hook
 ---
 
--- Hook to counter texture changes.
+-- Counters texture changes for artifact items.
 local function Hook_SetTexture(Region, Texture)
-	if Region.__ExitHook or Region.__MSQ_UnHook then
+	if Region.__ExitHook or not Region.__MSQ_Skin then
 		return
 	end
 
@@ -48,13 +58,12 @@ local function Hook_SetTexture(Region, Texture)
 	local Skin = Region.__MSQ_Skin
 	local SkinTexture = Skin.Texture
 
-	if Texture == [[Interface\Artifacts\RelicIconFrame]] then
+	if Texture == DEF_RELIC_TEXTURE then
 		SkinTexture = Skin.RelicTexture or SkinTexture or Texture
 		Region.__MSQ_Texture = Texture
-
 	else
-		SkinTexture = SkinTexture or [[Interface\Common\WhiteIconFrame]]
-		Region.__MSQ_Texture = [[Interface\Common\WhiteIconFrame]]
+		SkinTexture = SkinTexture or DEF_TEXTURE
+		Region.__MSQ_Texture = DEF_TEXTURE
 	end
 
 	Region:SetTexture(SkinTexture)
@@ -67,35 +76,35 @@ end
 
 -- Skins the 'IconBorder' region of a button.
 function SkinRegion.IconBorder(Region, Button, Skin, xScale, yScale)
-	local UnHook = Button.__MSQ_UnHook
-	Region.__MSQ_UnHook = UnHook
-
 	local Texture = Region.__MSQ_Texture or Region:GetTexture()
-	Region.__MSQ_Texture = Texture
-
 	local SkinTexture = Skin.Texture
 
-	if Texture == [[Interface\Artifacts\RelicIconFrame]] then
+	if Texture == DEF_RELIC_TEXTURE then
 		SkinTexture = Skin.RelicTexture or SkinTexture or Texture
 	else
-		SkinTexture = SkinTexture or [[Interface\Common\WhiteIconFrame]]
+		SkinTexture = SkinTexture or DEF_TEXTURE
 	end
 
-	Region.__MSQ_Skin = Skin
+	if Button.__MSQ_Enabled then
+		Region.__MSQ_Skin = Skin
+		Region.__MSQ_Texture = Texture
+
+		Hook_SetTexture(Region, SkinTexture)
+	else
+		Region.__MSQ_Skin = nil
+		Region.__MSQ_Texture = nil
+
+		Region:SetTexture(SkinTexture)
+	end
+
+	Region:SetTexCoord(GetTexCoords(Skin.TexCoords))
+	Region:SetBlendMode(Skin.BlendMode or "BLEND")
+	Region:SetDrawLayer(Skin.DrawLayer or "OVERLAY", Skin.DrawLevel or 0)
+	Region:SetSize(GetSize(Skin.Width, Skin.Height, xScale, yScale))
+	SetPoints(Region, Button, Skin, nil, Skin.SetAllPoints)
 
 	if not Region.__MSQ_Hooked then
 		hooksecurefunc(Region, "SetTexture", Hook_SetTexture)
 		Region.__MSQ_Hooked = true
 	end
-
-	Hook_SetTexture(Region, SkinTexture)
-	Region:SetTexCoord(GetTexCoords(Skin.TexCoords))
-
-	Region:SetBlendMode(Skin.BlendMode or "BLEND")
-
-	Region:SetDrawLayer(Skin.DrawLayer or "OVERLAY", Skin.DrawLevel or 0)
-
-	Region:SetSize(GetSize(Skin.Width, Skin.Height, xScale, yScale))
-
-	SetPoints(Region, Button, Skin, nil, Skin.SetAllPoints)
 end
