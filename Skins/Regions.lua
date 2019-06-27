134c7473275038dfd19f33335b9d47bd427d1cde
StormFX <stormfx@gmail.com>
2019-06-27 11:43:48 -0500

    - Update comments.

--- Skins/Regions.lua
+++ Skins/Regions.lua
@@ -6,11 +6,9 @@
 	* File...: Core\Skins\Regions.lua
 	* Author.: StormFX
 
-	Button Types
+	Regions Settings
 
 ]]
-
--- GLOBALS:
 
 local _, Core = ...
 
@@ -18,7 +16,8 @@ local _, Core = ...
 -- Region Settings
 
 -- * Key -> The button key that holds the region reference.
--- * Name -> The global key that holds the region reference.
+-- * Func -> The string suffix of the global key that holds the region reference.
+-- * Name -> The string name of the button method that returns the region reference.
 -- * Type -> Must match the game's internal type, for validation.
 
 -- * UseColor -> Use :SetColorTexture() if the skin provides a color but no texture.
@@ -27,8 +26,8 @@ local _, Core = ...
 -- * NoColor -> Do not allow color changes.
 -- * NoTexture -> Do not allow texture changes.
 
--- * Ignore -> Ignore when looking for regions.
--- * Iterate -> Include in iteration.
+-- * Ignore -> Ignore when looking for regions. @ Core\Group
+-- * Iterate -> Include in iteration. @ Core\Button
 ---
 
 do
