ac1325861c8544744fb31482dc9ea4beac8c1b81
StormFX <stormfx@gmail.com>
2020-10-13 10:39:38 -0500

    - Remove `HotKey.SetPoint` override. (Closes #116)

--- Core/Regions/Text.lua
+++ Core/Regions/Text.lua
@@ -25,21 +25,6 @@ local Defaults = Core.Skins.Default
 local GetSize, SetPoints = Core.GetSize, Core.SetPoints
 
 ----------------------------------------
--- Hook
----
-
--- Counter calls to SetPoint() after the HotKey region has been skinned.
-local function Hook_SetPoint(Region, ...)
-	if Region.__ExitHook or not Region.__MSQ_Skin then
-		return
-	end
-
-	Region.__ExitHook = true
-	SetPoints(Region, Region.__MSQ_Button, Region.__MSQ_Skin, Defaults.HotKey)
-	Region.__ExitHook = nil
-end
-
-----------------------------------------
 -- Core
 ---
 
@@ -57,25 +42,5 @@ function Core.SkinText(Region, Button, Layer, Skin, xScale, yScale)
 	Region:SetJustifyV(Skin.JustifyV or "MIDDLE")
 	Region:SetDrawLayer(Skin.DrawLayer or Default.DrawLayer)
 	Region:SetSize(GetSize(Skin.Width, Skin.Height or 10, xScale, yScale))
-
-	if Layer == "HotKey" then
-		if Button.__MSQ_Enabled then
-			Region.__MSQ_Skin = Skin
-			Region.__MSQ_Button = Button
-
-			if not Region.__MSQ_Hooked then
-				hooksecurefunc(Region, "SetPoint", Hook_SetPoint)
-				Region.__MSQ_Hooked = true
-			end
-
-			Hook_SetPoint(Region)
-		else
-			Region.__MSQ_Skin = nil
-			Region.__MSQ_Button = nil
-
-			SetPoints(Region, Button, Skin, Default)
-		end
-	else
-		SetPoints(Region, Button, Skin, Default)
-	end
+	SetPoints(Region, Button, Skin, Default)
 end
