c2bed78d5fddc4df8f16c4cf7dad34297bed6bac
StormFX <stormfx@gmail.com>
2022-10-31 17:53:23 -0500

    - Update koKR. (Netaras)

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -25,88 +25,88 @@ local L = Core.Locale
 
 L["About Masque"] = "Masque 정보"
 L["API"] = "API"
--- L["For more information, please visit one of the sites listed below."] = "For more information, please visit one of the sites listed below."
--- L["Masque is a skinning engine for button-based add-ons."] = "Masque is a skinning engine for button-based add-ons."
--- L["Select to view."] = "Select to view."
--- L["Supporters"] = "Supporters"
--- L["You must have an add-on that supports Masque installed to use it."] = "You must have an add-on that supports Masque installed to use it."
+L["For more information, please visit one of the sites listed below."] = "자세한 내용은 아래 나열된 사이트 중 하나를 방문해 주세요."
+L["Masque is a skinning engine for button-based add-ons."] = "Masque는 버튼 기반 애드온을 위한 스킨 엔진입니다."
+L["Select to view."] = "보려면 선택하세요."
+L["Supporters"] = "후원자"
+L["You must have an add-on that supports Masque installed to use it."] = "Masque를 사용하려면 Masque를 지원하는 애드온이 설치되어 있어야 합니다."
 
 ----------------------------------------
 -- Classic Skin
 ---
 
--- L["An improved version of the game's default button style."] = "An improved version of the game's default button style."
+L["An improved version of the game's default button style."] = "게임의 기본 버튼 스타일을 개선한 버전입니다."
 
 ----------------------------------------
 -- Core Settings
 ---
 
--- L["About"] = "About"
--- L["Click to load Masque's options."] = "Click to load Masque's options."
+L["About"] = "정보"
+L["Click to load Masque's options."] = "Masque 옵션을 불러오려면 클릭하세요."
 L["Load Options"] = "불러오기 옵션"
--- L["Masque's options are load on demand. Click the button below to load them."] = "Masque's options are load on demand. Click the button below to load them."
--- L["This action will increase memory usage."] = "This action will increase memory usage."
--- L["This section will allow you to view information about Masque and any skins you have installed."] = "This section will allow you to view information about Masque and any skins you have installed."
+L["Masque's options are load on demand. Click the button below to load them."] = "Masque의 옵션은 주문형 불러오기입니다. 불러려면 아래 버튼을 클릭하세요."
+L["This action will increase memory usage."] = "이 작업은 메모리 사용량을 증가시킵니다."
+L["This section will allow you to view information about Masque and any skins you have installed."] = "여기는 Masque 및 설치한 스킨에 대한 정보를 볼 수 있습니다."
 
 ----------------------------------------
 -- Default Classic Skin
 ---
 
--- L["The default Classic button style."] = "The default Classic button style."
+L["The default Classic button style."] = "기본 클래식 버튼 스타일입니다."
 
 ----------------------------------------
 -- Default Skin
 ---
 
--- L["The default button style."] = "The default button style."
+L["The default button style."] = "기본 버튼 스타일입니다."
 
 ----------------------------------------
 -- Developer Settings
 ---
 
 L["Causes Masque to throw Lua errors whenever it encounters a problem with an add-on or skin."] = "애드온이나 스킨에 문제가 생길 때마다 Masque가 Lua 오류를 발생시킵니다."
--- L["Clean Database"] = "Clean Database"
--- L["Click to purge the settings of all unused add-ons and groups."] = "Click to purge the settings of all unused add-ons and groups."
+L["Clean Database"] = "데이터베이스 정리"
+L["Click to purge the settings of all unused add-ons and groups."] = "사용하지 않는 모든 애드온 및 그룹 설정을 제거하려면 클릭하세요."
 L["Debug Mode"] = "디버그 모드"
 L["Developer"] = "개발자"
 L["Developer Settings"] = "개발자 설정"
 L["Masque debug mode disabled."] = "Masque 디버그 모드를 비활성화합니다."
 L["Masque debug mode enabled."] = "Masque 디버그 모드를 활성화합니다."
--- L["This action cannot be undone. Continue?"] = "This action cannot be undone. Continue?"
--- L["This section will allow you to adjust settings that affect working with Masque's API."] = "This section will allow you to adjust settings that affect working with Masque's API."
+L["This action cannot be undone. Continue?"] = "이 작업은 취소할 수 없습니다. 계속할까요?"
+L["This section will allow you to adjust settings that affect working with Masque's API."] = "여기는 Masque API 작업에 영향을 주는 설정을 조정할 수 있습니다."
 
 ----------------------------------------
 -- Dream Skin
 ---
 
--- L["A square skin with trimmed icons and a semi-transparent background."] = "A square skin with trimmed icons and a semi-transparent background."
+L["A square skin with trimmed icons and a semi-transparent background."] = "다듬은 아이콘과 반투명 배경이 있는 정사각형 스킨입니다."
 
 ----------------------------------------
 -- General Settings
 ---
 
 L["General Settings"] = "일반 설정"
--- L["This section will allow you to adjust Masque's interface and performance settings."] = "This section will allow you to adjust Masque's interface and performance settings."
+L["This section will allow you to adjust Masque's interface and performance settings."] = "여기는 Masque 인터페이스 및 성능 설정을 조정할 수 있습니다."
 
 ----------------------------------------
 -- Installed Skins
 ---
 
--- L["Author"] = "Author"
--- L["Authors"] = "Authors"
--- L["Click for details."] = "Click for details."
--- L["Compatible"] = "Compatible"
--- L["Description"] = "Description"
--- L["Incompatible"] = "Incompatible"
+L["Author"] = "제작자"
+L["Authors"] = "제작자"
+L["Click for details."] = "자세한 내용을 보려면 클릭하세요."
+L["Compatible"] = "호환"
+L["Description"] = "설명"
+L["Incompatible"] = "호환 불가"
 L["Installed Skins"] = "설치된 스킨"
--- L["No description available."] = "No description available."
+L["No description available."] = "가능한 설명이 없습니다."
 L["Status"] = "상태"
--- L["The status of this skin is unknown."] = "The status of this skin is unknown."
--- L["This section provides information on any skins you have installed."] = "This section provides information on any skins you have installed."
--- L["This skin is compatible with Masque."] = "This skin is compatible with Masque."
--- L["This skin is outdated and is incompatible with Masque."] = "This skin is outdated and is incompatible with Masque."
--- L["This skin is outdated but is still compatible with Masque."] = "This skin is outdated but is still compatible with Masque."
--- L["Unknown"] = "Unknown"
+L["The status of this skin is unknown."] = "이 스킨의 상태는 알 수 없습니다."
+L["This section provides information on any skins you have installed."] = "여기는 설치한 스킨에 대한 정보를 제공합니다."
+L["This skin is compatible with Masque."] = "이 스킨은 Masque와 호환됩니다."
+L["This skin is outdated and is incompatible with Masque."] = "이 스킨은 구식이며 Masque와 호환되지 않습니다."
+L["This skin is outdated but is still compatible with Masque."] = "이 스킨은 구식이지만 여전히 Masque와 호환됩니다."
+L["Unknown"] = "알 수 없음"
 L["Version"] = "버전"
 L["Website"] = "웹사이트"
 L["Websites"] = "웹사이트"
@@ -115,15 +115,13 @@ L["Websites"] = "웹사이트"
 -- Interface Settings
 ---
 
--- L["Classic Scaling"] = "Classic Scaling"
 L["Enable the Minimap icon."] = "미니맵 아이콘을 활성화 합니다."
 L["Interface"] = "인터페이스"
 L["Interface Settings"] = "인터페이스 설정"
 L["Minimap Icon"] = "미니맵 아이콘"
--- L["Stand-Alone GUI"] = "Stand-Alone GUI"
--- L["This section will allow you to adjust settings that affect Masque's interface."] = "This section will allow you to adjust settings that affect Masque's interface."
--- L["Use a resizable, stand-alone options window."] = "Use a resizable, stand-alone options window."
--- L["Use the old scaling method for action buttons."] = "Use the old scaling method for action buttons."
+L["Stand-Alone GUI"] = "독립형 GUI"
+L["This section will allow you to adjust settings that affect Masque's interface."] = "여기는 Masque 인터페이스에 영향을 주는 설정을 조정할 수 있습니다."
+L["Use a resizable, stand-alone options window."] = "크기 조정 가능한 독립 실행형 옵션 창을 사용합니다."
 
 ----------------------------------------
 -- LDB Launcher
@@ -135,14 +133,14 @@ L["Minimap Icon"] = "미니맵 아이콘"
 -- Performance Settings
 ---
 
--- L["Click to load reload the interface."] = "Click to load reload the interface."
--- L["Load the skin information panel."] = "Load the skin information panel."
+L["Click to load reload the interface."] = "인터페이스를 다시 불러오려면 클릭하세요."
+L["Load the skin information panel."] = "스킨 정보 패널을 불러옵니다."
 L["Performance"] = "성능"
 L["Performance Settings"] = "성능 설정"
--- L["Reload Interface"] = "Reload Interface"
--- L["Requires an interface reload."] = "Requires an interface reload."
+L["Reload Interface"] = "인터페이스 새로 고침"
+L["Requires an interface reload."] = "인터페이스 다시 불러와야 합니다."
 L["Skin Information"] = "스킨 정보"
--- L["This section will allow you to adjust settings that affect Masque's performance."] = "This section will allow you to adjust settings that affect Masque's performance."
+L["This section will allow you to adjust settings that affect Masque's performance."] = "여기는 Masque 성능에 영향을 미치는 설정을 조정할 수 있습니다."
 
 ----------------------------------------
 -- Profile Settings
@@ -154,7 +152,8 @@ L["Profile Settings"] = "프로필 설정"
 -- Skin Settings
 ---
 
-L["Backdrop"] = "뒷배경 설정"
+-- L["Adjust the scale of this group's skin."] = "Adjust the scale of this group's skin."
+L["Backdrop"] = "배경"
 L["Checked"] = "선택된 버튼"
 L["Color"] = "색상"
 L["Colors"] = "색상"
@@ -163,41 +162,43 @@ L["Disable"] = "비활성화"
 L["Disable the skinning of this group."] = "이 그룹의 스킨 씌우기를 비활성화합니다."
 L["Disabled"] = "비활성화된 버튼"
 L["Enable"] = "활성화"
+-- L["Enable skin scaling."] = "Enable skin scaling."
 L["Enable the Backdrop texture."] = "바탕 텍스쳐를 활성화합니다."
 -- L["Enable the Gloss texture."] = "Enable the Gloss texture."
 -- L["Enable the Shadow texture."] = "Enable the Shadow texture."
 L["Flash"] = "번쩍임"
-L["Global"] = "공통 옵션"
--- L["Global Settings"] = "Global Settings"
+L["Global"] = "공통"
+L["Global Settings"] = "공통 설정"
 L["Gloss"] = "광택 효과 설정"
 L["Highlight"] = "강조된 버튼"
 L["Normal"] = "평상 시 버튼"
--- L["Pulse"] = "Pulse"
+L["Pulse"] = "맥박"
 L["Pushed"] = "눌려진 버튼"
-L["Reset all skin options to the defaults."] = "모든 색상을 기본값으로 초기화 합니다."
+L["Reset all skin options to the defaults."] = "모든 스킨 옵션을 기본값으로 재설정합니다."
 L["Reset Skin"] = "스킨 초기화"
-L["Set the color of the Backdrop texture."] = "바탕 텍스쳐의 색상을 설정합니다."
+-- L["Scale"] = "Scale"
+L["Set the color of the Backdrop texture."] = "배경 텍스처의 색상을 설정합니다."
 L["Set the color of the Checked texture."] = "선택된 버튼 텍스처의 색상을 변경합니다."
-L["Set the color of the Cooldown animation."] = "재사용 대기시간 움직임의 색상을 변경합니다."
+L["Set the color of the Cooldown animation."] = "재사용 대기시간 애니메이션의 색상을 설정합니다."
 L["Set the color of the Disabled texture."] = "비활성화된 버튼 텍스처의 색상을 변경합니다."
 L["Set the color of the Flash texture."] = "번쩍임 텍스처의 색상을 변경합니다."
 L["Set the color of the Gloss texture."] = "광택 효과 텍스처의 색상을 변경합니다."
 L["Set the color of the Highlight texture."] = "강조된 버튼 텍스처의 색상을 변경합니다."
 L["Set the color of the Normal texture."] = "평상 시 버튼 텍스처의 색상을 변경합니다."
 L["Set the color of the Pushed texture."] = "눌려진 버튼 텍스처의 색상을 변경합니다."
--- L["Set the color of the Shadow texture."] = "Set the color of the Shadow texture."
-L["Set the skin for this group."] = "이 그룹을 위한 스킨을 설정합니다."
--- L["Shadow"] = "Shadow"
--- L["Show the pulse effect when a cooldown finishes."] = "Show the pulse effect when a cooldown finishes."
+L["Set the color of the Shadow texture."] = "그림자 텍스처의 색상을 설정합니다."
+L["Set the skin for this group."] = "이 그룹의 스킨을 설정합니다."
+L["Shadow"] = "그림자"
+L["Show the pulse effect when a cooldown finishes."] = "재사용 대기시간이 끝나면 맥박 효과를 표시합니다."
 L["Skin"] = "스킨"
 L["Skin Settings"] = "스킨 설정"
--- L["This section will allow you to adjust the skin settings of all buttons registered to %s."] = "This section will allow you to adjust the skin settings of all buttons registered to %s."
--- L["This section will allow you to adjust the skin settings of all buttons registered to %s. This will overwrite any per-group settings."] = "This section will allow you to adjust the skin settings of all buttons registered to %s. This will overwrite any per-group settings."
--- L["This section will allow you to adjust the skin settings of all registered buttons. This will overwrite any per-add-on settings."] = "This section will allow you to adjust the skin settings of all registered buttons. This will overwrite any per-add-on settings."
-L["This section will allow you to skin the buttons of the add-ons and add-on groups registered with Masque."] = "여기서는 Masque에 등록된 애드온 및 애드온 그룹의 버튼에 스킨을 씌울 수 있습니다."
+L["This section will allow you to adjust the skin settings of all buttons registered to %s."] = "여기는 %s에 등록된 모든 버튼의 스킨 설정을 조정할 수 있습니다."
+L["This section will allow you to adjust the skin settings of all buttons registered to %s. This will overwrite any per-group settings."] = "여기는 %s에 등록된 모든 버튼의 스킨 설정을 조정할 수 있습니다. 이는 그룹별 설정을 덮어씁니다."
+L["This section will allow you to adjust the skin settings of all registered buttons. This will overwrite any per-add-on settings."] = "여기는 등록된 모든 버튼의 스킨 설정을 조정할 수 있습니다. 이는 애드온별 설정을 덮어씁니다."
+L["This section will allow you to skin the buttons of the add-ons and add-on groups registered with Masque."] = "여기는 Masque에 등록된 애드온 및 애드온 그룹의 버튼에 스킨을 씌울 수 있습니다."
 
 ----------------------------------------
 -- Zoomed Skin
 ---
 
--- L["A square skin with zoomed icons and a semi-transparent background."] = "A square skin with zoomed icons and a semi-transparent background."
+L["A square skin with zoomed icons and a semi-transparent background."] = "확대된 아이콘과 반투명 배경이 있는 정사각형 스킨입니다."
