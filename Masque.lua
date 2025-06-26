0c7a23b320b9b40f621d4a2fb27bc7539f33273e
StormFX <stormfx@gmail.com>
2025-06-26 10:10:10 -0500

    - Bump retail version.

--- Masque.lua
+++ Masque.lua
@@ -45,7 +45,7 @@ local API_VERSION = 110000
 
 -- Client Version
 local WOW_VERSION = select(4, GetBuildInfo()) or 0
-local WOW_RETAIL = ((WOW_VERSION > 100000) and true) or nil
+local WOW_RETAIL = ((WOW_VERSION > 110000) and true) or nil
 
 ----------------------------------------
 -- Utility
