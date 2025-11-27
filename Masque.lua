08952440e6e0c4b463f041c4f6b223d3ebadbfa3
StormFX <stormfx@gmail.com>
2025-11-27 09:50:37 -0600

    - `API_VERSION` to `110207`.

--- Masque.lua
+++ Masque.lua
@@ -41,7 +41,7 @@ local L = Core.Locale
 local Masque = LibStub("AceAddon-3.0"):NewAddon(MASQUE)
 
 -- API Version
-local API_VERSION = 110107
+local API_VERSION = 110207
 
 -- Client Version
 local WOW_VERSION = select(4, GetBuildInfo()) or 0
