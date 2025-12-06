9d5be822e92592a89febddd71c58c036c85196e2
StormFX <stormfx@gmail.com>
2025-12-05 19:17:21 -0600

    - Update Defaults.

--- Skins/Defaults.lua
+++ Skins/Defaults.lua
@@ -16,6 +16,15 @@ local _, Core = ...
 -- Locals
 ---
 
+-- Base Paths
+local PATH_BASE = [[Interface\AddOns\Masque\Textures\]]
+local PATH_BACKDROP = PATH_BASE..[[Backdrop\]]
+
+-- Backdrop Paths
+local BACKDROP_ACTION = PATH_BACKDROP.."Action"
+local BACKDROP_AURA = PATH_BACKDROP.."Aura"
+local BACKDROP_ITEM = PATH_BACKDROP.."Action"
+
 local Defaults = {
 	-- [ Shared ]
 	-- TexCoords = {0, 1, 0, 1}, -- @ Core/Utility/GetTexCoords
@@ -29,6 +38,28 @@ local Defaults = {
 	RelPoint = "CENTER",
 	OffsetX = 0,
 	OffsetY = 0,
+	Backdrop = {
+		Texture = PATH_BACKDROP.."Slot-Modern",
+		Textures = {
+			Action = BACKDROP_ACTION,
+			Aura = BACKDROP_AURA,
+			Backpack = BACKDROP_ITEM,
+			BagSlot = BACKDROP_ITEM,
+			Buff = BACKDROP_AURA,
+			Debuff = PATH_BACKDROP.."Debuff",
+			Enchant = PATH_BACKDROP.."Enchant",
+			Item = BACKDROP_ITEM,
+			KeyRing = BACKDROP_ITEM,
+			Pet = PATH_BACKDROP.."Pet",
+			Possess = BACKDROP_ACTION,
+			ReagentBag = BACKDROP_ITEM,
+			Stance = BACKDROP_ACTION,
+		},
+		Color = {0, 0, 0, 0.5}, -- Color Texture Only
+		BlendMode = "BLEND",
+		DrawLayer = "BACKGROUND",
+		DrawLevel = -1,
+	},
 }
 
 ----------------------------------------
